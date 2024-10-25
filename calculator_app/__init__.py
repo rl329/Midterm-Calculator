d178021463e66c8767bc370d3c3b9f7a0c66bef0
rl329 <rl329@njit.edu>
2024-10-24 19:38:01 -0500

    Cleaned up my files by changing from uppercase to lowercase

---
+++ calculator_app/__init__.py
@@ -0,0 +1,9 @@
+"""This module initializes the Calculator App."""
+
+def add(a, b):
+    """Add two numbers."""
+    return a + b
+
+def subtract(a, b):
+    """Subtract two numbers."""
+    return a - b
